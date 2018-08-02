c6d95f0c20a1c6652faccbedac5b65b18d66790a
Saleem <mdsaleemj@gmail.com>
2018-08-02 17:23:40 +0530

    adding typography utils for blog global styles

---
+++ src/utils/typography.js
@@ -0,0 +1,12 @@
+import Typography from 'typography'
+import Wordpress2016 from 'typography-theme-wordpress-2016'
+
+const typography = new Typography(Wordpress2016)
+
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== 'production') {
+    typography.injectStyles()
+  }
+
+console.log(typography);
+export default typography
